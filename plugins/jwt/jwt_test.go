890ee07051488dc51d9a1fb0c6ff7f81f055a95a
Andriy Romanov <judo.ras@gmail.com>
2016-05-21 01:02:41 -0700

    Added first jwt config parsing test

--- plugins/jwt/jwt_test.go
+++ plugins/jwt/jwt_test.go
@@ -5,5 +5,12 @@ import (
 )
 
 func TestParseConfig(t *testing.T) {
-
+	jwtPlugin := &JWT{}
+	err := jwtPlugin.ParseConfig("test_config/jwt.toml")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(jwtPlugin.Secret) != "secret123" {
+		t.Errorf("Secret key was parsed incorrectly. Expected: '%v', but got: '%v'", "secret123", string(jwtPlugin.Secret))
+	}
 }
